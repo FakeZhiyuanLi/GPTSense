22bee7e9293c480be8c30607995753852731fc14
liz-mvla <100027902@mvla.net>
2023-04-09 04:31:36 -0700

    changed formatting

--- src/main/db/utils/fileNames.py
+++ src/main/db/utils/fileNames.py
@@ -1,7 +1,7 @@
 import os
 
-NGQFolder = os.path.abspath(os.path.join(__file__, '..', '..', '..', '..', '..', 'datasets', 'NGQ'))
-certificationFolder = os.path.abspath(os.path.join(__file__, '..', '..', '..', '..', '..', 'CertificationFiles'))
+NGQFolder = os.path.abspath(os.path.join(__file__, "..", "..", "..", "..", "..", "datasets", "NGQ"))
+certificationFolder = os.path.abspath(os.path.join(__file__, "..", "..", "..", "..", "..", "CertificationFiles"))
 
 tgt_train = os.path.abspath(os.path.join(NGQFolder, "tgt-train.txt"))
 tgt_dev = os.path.abspath(os.path.join(NGQFolder, "tgt-dev.txt"))
